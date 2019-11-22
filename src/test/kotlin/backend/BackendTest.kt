ac33f7a214a47766d78cce468c0aea8a08af893f
LorisUnibo <loris.cangini@studio.unibo.it>
2019-11-22 15:07:01 +0100

    test fail test

--- src/test/kotlin/backend/BackendTest.kt
+++ src/test/kotlin/backend/BackendTest.kt
@@ -17,6 +17,6 @@ internal class BackendTest {
     fun getNeighbours() {
         assertEquals(Backend.getNeighbours(devices[1]), setOf(devices[0]))
         Backend.subscribe(devices[1], setOf(devices[0], devices[2]))
-        assertEquals(Backend.getNeighbours(devices[1]), setOf(devices[0]))
+        assertEquals(Backend.getNeighbours(devices[1]), setOf(devices[1]))
     }
 }

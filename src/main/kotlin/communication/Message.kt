1d8e47cc3be083f9c4425c446ffcad14297c4e21
Matteo Zattoni <matteo.zattoni@icloud.com>
2020-05-22 16:57:37 +0200

    Update Message.kt
    Added new Message Type: OfferServer and now the senderUid is a DeviceUID instead of Int

--- src/main/kotlin/communication/Message.kt
+++ src/main/kotlin/communication/Message.kt
@@ -1,7 +1,8 @@
 package communication
 
+import org.protelis.lang.datatype.DeviceUID
 import java.io.Serializable
 
-enum class MessageType : Serializable { Execute, Result, Show, Join, ID, Status, SendToNeighbours, GoLightWeight, LeaveLightWeight }
+enum class MessageType : Serializable { Execute, Result, Show, Join, OfferServer, ID, Status, SendToNeighbours, GoLightWeight, LeaveLightWeight }
 
-data class Message(val senderUid: Int, val type: MessageType, val content: Serializable? = null) : Serializable
+data class Message(val senderUid: DeviceUID, val type: MessageType, val content: Serializable? = null) : Serializable

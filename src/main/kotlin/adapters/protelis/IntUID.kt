357d13bcb34c3b17532fe991e2e92bc08fc3b77e
Matteo Zattoni <matteo.zattoni@icloud.com>
2020-05-22 16:53:10 +0200

    Removed old class

--- src/main/kotlin/adapters/protelis/IntUID.kt
+++
@@ -1,8 +0,0 @@
-package adapters.protelis
-
-import org.protelis.lang.datatype.DeviceUID
-
-data class IntUID(private val uid: Int) : DeviceUID, Comparable<IntUID> {
-    override fun compareTo(other: IntUID) = uid.compareTo(other.uid)
-    fun getUID() = uid
-}

619222ae0ff32eaed1d5547345f4745fff0aaabd
Matteo Zattoni <matteo.zattoni@icloud.com>
2020-05-22 16:51:59 +0200

    Removed Scala from repository
    Compatibily with scala is not supported anymore until the problem get fixed, caused because now the Device is not using a Integer for identify itself

--- src/main/scala/adapters/scafi/AbstractAggregateProgram.scala
+++
@@ -1,7 +0,0 @@
-package adapters.scafi
-
-/**
- * The only purpose for this class is to permit katlin to define Aggregate Programs making use
- * of all the needed implicits
- */
-abstract class AbstractAggregateProgram extends ScafiIncarnation.AggregateProgram
--- src/main/scala/adapters/scafi/ScafiIncarnation.scala
+++
@@ -1,7 +0,0 @@
-package adapters.scafi
-
-import it.unibo.scafi.incarnations.BasicAbstractIncarnation
-
-object ScafiIncarnation extends BasicAbstractIncarnation with Serializable {
-  def emptyContext(id: ID): ScafiIncarnation.CONTEXT = ScafiIncarnation.factory.context(id, Map())
-}
